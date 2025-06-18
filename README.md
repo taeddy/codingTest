763237a37207e3531b74ed00362ec7f1f9d12b99
taeddy <stz3148@naver.com>
2025-06-18 11:13:23 +0900

    update count_problem_source

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-## 코딩 1일 1문제! ( CODING TEST PRACTICE )
-[![SOMJANG LOGO](/images/SOMJANG.png)](https://somjang.tistory.com/category/Programming/%EC%BD%94%EB%94%A9%201%EC%9D%BC%201%EB%AC%B8%EC%A0%9C)
-## 하루에 한 문제 씩이라도 코딩문제를 풀어보자! 
-### Since 2020.02.07 ~
-#### 모든 문제는 Python3 로 해결하였습니다.
+## 코딩 알고리즘 문제 풀이 모음
+
 #### 현재까지 풀어본 총 문제 수 : 0개
 
 #### 아래의 페이지에서 제공하는 문제들로 구성되어 있습니다.
-[![BaekJoon](/images/BaekJoon.png)](https://www.acmicpc.net/)
-[![Programmers](/images/Programmers.png)](https://programmers.co.kr/)
-[![Samsung_SW_Academy](/images/Samsung_SW_Academy.png)](https://swexpertacademy.com/main/main.do)
-[![LeetCode](/images/LeetCode.png)](https://leetcode.com/)
-[![HackerRank](/images/HackerRank.png)](https://www.hackerrank.com/)
-<p align="center"><a href="http://www.jungol.co.kr/"><img src="/images/JUNGOL.png"></a></p>
-<p align="center"><a href="https://codeup.kr/"><img src="/images/CodeUp.png"></a></p>
+[BaekJoon(Solved.ac)](https://solved.ac/en/profile/stz3148)
+[Programmers](https://programmers.co.kr/)
+[Codeforces](https://codeforces.com/profile/Taeddy)
+[Atcoder](https://atcoder.jp/)
+[Samsung_SW_Academy](https://swexpertacademy.com/main/main.do)
+[LeetCode](https://leetcode.com/)
+[HackerRank](https://www.hackerrank.com/)
+[Jungol](http://www.jungol.co.kr/)
+[Codeup](https://codeup.kr/)
--- utils/count_problem_source.py
+++ utils/count_problem_source.py
@@ -3,53 +3,46 @@ from datetime import datetime
 import os
 import re
 
+DIRECTORY_LIST = ['solved_ac', 'programmers', 'codeforces']
+
 
 def count_problem_source_code():
-    problem_solve_code_list = []
+    total_code_num = 0
+    code_cnt_info = {}
 
-    directory_list = [directory for directory in os.listdir("./") if "DAY" in directory]
-
-    for directory in directory_list:
+    for directory in DIRECTORY_LIST:
         code_list = os.listdir(f"./{directory}")
-
-        problem_solve_code_list += code_list
-
-    name_list = [re.findall(r'\[[^)]*\]', code_name) for code_name in problem_solve_code_list]
-
-    name_list = [name[0].replace("[", "").replace("]", "") for name in name_list if len(name) > 0]
-
-    total_code_num = len(name_list)
-    
-    code_cnt_info = sorted(Counter(name_list).items(), key=lambda x: -x[1])
+        total_code_num += len(code_list)
+        code_cnt_info[directory] = len(code_list)
 
     return total_code_num, code_cnt_info
 
 
-def make_count_info(total_code_num, code_cnt_info):
+def make_count_info(total_code_num: int, code_cnt_info: dict):
     count_info = f"#### 현재까지 풀어본 총 문제 수 : {total_code_num}개\n"
 
-    for name in code_cnt_info:
-        temp = f"- {name[0]} - {name[1]}개\n"
+    for name, cnt in code_cnt_info.items():
+        temp = f"- {name} - {cnt}개\n"
         count_info += temp
 
     return count_info
 
 
 def make_read_me(count_info):
-    return f"""## 코딩 1일 1문제! ( CODING TEST PRACTICE )
-[![SOMJANG LOGO](/images/SOMJANG.png)](https://somjang.tistory.com/category/Programming/%EC%BD%94%EB%94%A9%201%EC%9D%BC%201%EB%AC%B8%EC%A0%9C)
-## 하루에 한 문제 씩이라도 코딩문제를 풀어보자! 
-### Since 2020.02.07 ~
-#### 모든 문제는 Python3 로 해결하였습니다.
+    return f"""## 코딩 알고리즘 문제 풀이 모음
+
 {count_info}
+
 #### 아래의 페이지에서 제공하는 문제들로 구성되어 있습니다.
-[![BaekJoon](/images/BaekJoon.png)](https://www.acmicpc.net/)
-[![Programmers](/images/Programmers.png)](https://programmers.co.kr/)
-[![Samsung_SW_Academy](/images/Samsung_SW_Academy.png)](https://swexpertacademy.com/main/main.do)
-[![LeetCode](/images/LeetCode.png)](https://leetcode.com/)
-[![HackerRank](/images/HackerRank.png)](https://www.hackerrank.com/)
-<p align="center"><a href="http://www.jungol.co.kr/"><img src="/images/JUNGOL.png"></a></p>
-<p align="center"><a href="https://codeup.kr/"><img src="/images/CodeUp.png"></a></p>"""
+[BaekJoon(Solved.ac)](https://solved.ac/en/profile/stz3148)
+[Programmers](https://programmers.co.kr/)
+[Codeforces](https://codeforces.com/profile/Taeddy)
+[Atcoder](https://atcoder.jp/)
+[Samsung_SW_Academy](https://swexpertacademy.com/main/main.do)
+[LeetCode](https://leetcode.com/)
+[HackerRank](https://www.hackerrank.com/)
+[Jungol](http://www.jungol.co.kr/)
+[Codeup](https://codeup.kr/)"""
 
 
 def update_readme_md():
