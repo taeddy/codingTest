ba5e9e15b7e579dc43b3a43bfeebff8cebaf5e3b
taeddy <stz3148@naver.com>
2025-07-06 18:29:17 +0900

    250706

--- solved_ac/12185.py
+++ solved_ac/12185.py
@@ -3,40 +3,32 @@
 
 
 def solution():
-    N = int(input())
-    bus_ranges_str = input().split()
-    bus_ranges = []
-    for i in range(0, 2 * N, 2):
-        bus_ranges.append((int(bus_ranges_str[i]), int(bus_ranges_str[i + 1])))
+    T = int(input())
+    for testcase in range(T):
+        N = int(input())
+        buses = []
+        line = list(map(int, input().split()))
+        for idx in range(len(line) // 2):
+            buses.append((line[2 * idx], line[2 * idx + 1]))
 
-    P = int(input())
-    query_cities = []
-    for _ in range(P):
-        query_cities.append(int(input()))
+        can_serve_cnt = [0] * 5001
+        for start, end in buses:
+            for city in range(start, end + 1):
+                can_serve_cnt[city] += 1
 
-    # Max city number is 5000 based on problem constraints (implicitly)
-    # So we need an array up to 5001 (index 0 to 5000)
-    city_counts = [0] * 5002
+        P = int(input())
+        want_to_know_cities = []
+        for _ in range(P):
+            want_to_know_cities.append(int(input()))
+        result = []
+        for city in want_to_know_cities:
+            result.append(can_serve_cnt[city])
 
-    for start, end in bus_ranges:
-        city_counts[start] += 1
-        if end + 1 <= 5001:  # Ensure we don't go out of bounds
-            city_counts[end + 1] -= 1
+        print(f"Case #{testcase+1}:", end=" ")
+        print(" ".join(map(str, result)))
 
-    # Calculate prefix sums
-    for i in range(1, 5002):
-        city_counts[i] += city_counts[i - 1]
-
-    results = []
-    for city in query_cities:
-        results.append(str(city_counts[city]))
-
-    return " ".join(results)
+        if testcase < T - 1:
+            tmp = input()
 
 
-T = int(input())
-for i in range(1, T + 1):
-    result = solution()
-    print(f"Case #{i}: {result}")
-    if i < T:  # Handle the blank line separation for Kickstart
-        input()
+solution()
