7c666e7108fbcb550d0496219f6c97d2fcd365fd
taeddy <stz3148@naver.com>
2025-07-10 14:25:54 +0900

    250710

---
+++ programmers/131120.sql
@@ -0,0 +1,7 @@
+-- 3월에 태어난 여성 회원 목록 출력하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131120
+
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d')
+FROM MEMBER_PROFILE
+WHERE GENDER = 'W' AND MONTH(DATE_OF_BIRTH) = 3 AND TLNO IS NOT NULL
+ORDER BY MEMBER_ID
