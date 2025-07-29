bc61313798787fb9b055c6e032439e13fdb3a162
taeddy <stz3148@naver.com>
2025-07-29 16:33:59 +0900

    250729

--- solved_ac/21604.py
+++ solved_ac/21604.py
@@ -1,53 +1,23 @@
 # 겹강 찾기
 # https://www.acmicpc.net/problem/21604
 
-# 한 개의 수업을 함께 수업을 듣는 사람이 한 쌍도 없었습니다 >>>>        N >= M
-# K <= M
-# 단, 싸이컴회원+상상친구 중 분반이 겹치는 인원이 있어서는 안됨.
+# 이미 짜여진 회원의 커리큘럼과 같은 N*M을 만들고, 각 열을 rotate하면 됨.
 
-import itertools
-import random
+def solve():
+    import sys
+    input = sys.stdin.readline
 
-def count_same_elements(l1, l2):
-    cnt = 0
-    for e1, e2 in zip(l1, l2):
-        if e1 == e2:
-            cnt += 1
-    return cnt
+    N, M = map(int, input().split())
+    A = [list(map(int, input().split())) for _ in range(M)]
 
-def can_be_added(new, imaginary_friends):
-    for old in imaginary_friends:
-        cnt = count_same_elements(new, old)
-        if cnt > 0:
-            return False
-    return True
+    # 가상 친구 수는 M명
+    print(M)
+    # B[i][j] = A[(i + j) % M][j]
+    for i in range(M):
+        row = []
+        for j in range(N):
+            row.append(A[(i + j) % M][j])
+        print(*row)
 
-# 입력 처리
-N, M = map(int, input().split())
-classes = [list(map(int, input().split())) for _ in range(M)]
-
-# 분반 정보를 string으로 저장 (비교를 편리하게 만듦)
-division_dict = []
-for _class in classes:
-    division_dict.append(''.join(map(str, _class)))
-
-imaginary_friends = []
-# 상상 친구 생성
-while True:
-    imaginary_friends = []
-    friends = list(itertools.product(range(1, N+1), repeat=N))
-    while True:
-        friend = random.choice(friends)
-        if len(imaginary_friends) == M:
-            break
-        joined = ''.join(map(str, friend))
-        if joined not in division_dict and can_be_added(friend, imaginary_friends):
-            imaginary_friends.append(friend)
-            
-    if len(imaginary_friends) == M:
-        break
-
-# 출력
-print(len(imaginary_friends))
-for friend in imaginary_friends:
-    print(' '.join(map(str, friend)))
+if __name__ == "__main__":
+    solve()
