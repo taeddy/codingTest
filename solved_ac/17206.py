d7778d9c1187705b6ab6c98bc7fa2e3614b19752
taeddy <stz3148@naver.com>
2025-08-07 00:02:08 +0900

    250807

--- solved_ac/17206.py
+++ solved_ac/17206.py
@@ -8,32 +8,11 @@ input = sys.stdin.readline
 T = int(input())
 Ns = list(map(int, input().split()))
 
-li = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 3,
-    4: 3,
-    5: 3,
-    6: 9,
-    7: 16,
-    8: 16,
-    9: 25,
-    10: 25,
-    11: 25,
-    12: 37,
-    13: 37,
-    14: 51,
-    15: 66,
-    16: 66,
-    17: 66,
-    18: 84,
-    19: 84,
-    20: 84,
-}
+
+def cal_sum(n, x):
+    return n * (n + 1) // 2 * x
+
 
 for N in Ns:
-    share = N // 21
-    rem = N % 21
-    ans = 189 * share * (share + 1) / 2 + share * (105 - 189) + li[rem]
-    print(int(ans))
+    ans = cal_sum(N // 3, 3) + cal_sum(N // 7, 7) - cal_sum(N // 21, 21)
+    print(ans)
